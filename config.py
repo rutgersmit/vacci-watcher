db0206dc61b72a908f47201b1969e584b815c280
Rutger <rutger.smit@gmail.com>
2021-05-14 20:57:23 +0200

    Config update

--- config.py
+++ config.py
@@ -1,39 +1,28 @@
-import os
-
-selenium_host = os.environ['REMOTE_SELENIUM_HOSTNAME']
-token = os.environ['TELEGRAM_TOKEN']
-chatid = os.environ['TELEGRAM_CHATID']
-city = os.environ['VACCINATION_CITY']
-
-interval = os.environ['INTERVAL']
-if not interval.isnumeric():
-    interval = None
-else:
-    interval = int(interval)
-
-interval_standby = os.environ['INTERVAL_STANDBY']
-if not interval_standby.isnumeric():
-    interval_standby = None
-else:
-    interval_standby = int(interval_standby)
-
-
 if not selenium_host:
     print('⚠ No Selenium hostname (SELENIUM_REMOTE_HOSTNAME) specified in the environment variables. Falling back to localhost.')
     selenium_host = 'localhost'
+else:
+    print('Selenium host: {}'.format(selenium_host))
 
 if not token:
     print('⚠ No Telegram token (TELEGRAM_TOKEN) specified in the environment variables. No alerts will be sent.')
+else:
+    print('Telegram token: {}'.format(token))
 
 if not chatid:
-    print('⚠ No Telegram chat id (TELEGRAM_CHATID) specified in the environment variables. No alerts will be sent.')   
+    print('⚠ No Telegram chat id (TELEGRAM_CHATID) specified in the environment variables. No alerts will be sent.')
+else:
+    print('Telegram chatid: {}'.format(chatid))
 
 if not interval:
-    print('No interval (INTERVAL) specified in the environment variables. Defaulting to 60 seconds.')   
+    print('No interval (INTERVAL) specified in the environment variables. Defaulting to 60 seconds.')
     interval = 60
+else:
+    print('Check interval: {}'.format(interval))
+
 
 if not interval_standby:
-    print('No interval during standby (INTERVAL_STANDBY) specified in the environment variables. Defaulting to 300 seconds.')   
+    print('No interval during standby (INTERVAL_STANDBY) specified in the environment variables. Defaulting to 300 seconds.')
     interval = 300
-
-
+else:
+    print('Standby interval: {}'.format(token))
